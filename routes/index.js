1021c381e1b8bf226ddcd102a61516318b03cfd8
aizealawin <ktmudalige@gmail.com>
2023-10-26 12:11:36 -0400

    init routes

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const { Router } = require('express')
+const router = Router()
+
+const controllers = require('../controllers')
+
+router.get('/', (req, res) => res.send('this is root.'))
+
+module.exports = router
