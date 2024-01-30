21c01262d4cab980fa0d2d6ad191aff2fceee1a7
aizealawin <ktmudalige@gmail.com>
2024-01-30 15:39:22 -0500

    CRUD tracker controller set up

--- controllers/trackerController.js
+++ controllers/trackerController.js
@@ -23,9 +23,53 @@ const getAllTrackers = async (req, res) => {
   }
 }
 
+const getTrackerById = async (req, res) => {
+  try { 
+    const { id } = req.params
+    const tracker = await Tracker.findById(id)
+    if (tracker) { 
+      return res.status(200).json({
+        tracker
+      })
+      return res.status(404).send("Tracker with specific ID does not exist")
+    }
+  } catch (error) { 
+    return res.status(500).json({error: error.message})
+  }
+}
+
+const findByIdAndDeleteTracker = async (req, res) => {
+  try {
+    const { id } = req.params
+    const tracker = await Tracker.findByIdAndDelete(id)
+    if (tracker) {
+      return res.status.send('Tracker deleted!')
+    }
+    return res.status(404).send("Tracker with specific ID does not exist.")
+  } catch (error) { 
+    return res.status(500).json({ error: error.message})
+  }
+}
+
+const findByIdAndUpdateTracker = async (req, res) => {
+  try { 
+    const { id } = req.params
+    const tracker = await Tracker.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
+    res.status(200).json(tracker)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 
 module.exports = {
   postTracker,
   getAllTrackers,
+  getTrackerById,
+  findByIdAndDeleteTracker,
+  findByIdAndUpdateTracker,
+
 
 }
